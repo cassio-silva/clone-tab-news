0147fa337c99c81fc66229238456109c07da2330
cassio-silva <cassioglopesdasilva@hotmail.com>
2024-11-29 17:54:40 -0300

    Trata a conexao ssl do banco

--- infra/database.js
+++ infra/database.js
@@ -7,18 +7,8 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("Credenciais do Postgres: ", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-
-  // client.on("error", (err) => {
-  //   console.error("database error", err.stack);
-  // });
 
   try {
     await client.connect();
