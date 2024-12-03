8b806a0bdfb30aec19ce903a795a7013ae48e415
cassio-silva <cassioglopesdasilva@hotmail.com>
2024-12-03 13:57:40 -0300

    Adiciona o endpoint `/api/v1/migrations`

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,32 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+
+test("POST para /api/v1/migrations deve retornar 200", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response1.status).toBe(201);
+
+  const response1Body = await response1.json();
+
+  expect(Array.isArray(response1Body)).toBe(true);
+  expect(response1Body.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const response2Body = await response2.json();
+  const query2 = await database.query({
+    text: "SELECT COUNT(*)::int FROM pgmigrations",
+  });
+
+  expect(query2.rows[0].count).toBeGreaterThan(0);
+  expect(response2Body.length).toBe(0);
+  expect(Array.isArray(response2Body)).toBe(true);
+});
